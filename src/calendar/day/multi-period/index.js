4d9cadcf2dd0b3dc563570891f15a7a8d6fc209f
Pereira Neto <31260462+pereiraneto@users.noreply.github.com>
2020-07-27 13:22:55 -0300

    Update index.js

--- src/calendar/day/multi-period/index.js
+++ src/calendar/day/multi-period/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import PropTypes from 'prop-types';
 import {shouldUpdate} from '../../../component-updater';
 
